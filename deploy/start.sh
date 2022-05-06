80efd6dabf1ee3a848ac833e922d671a7a8431be
kiwon kwon <funnykyeon@naver.com>
2022-05-06 09:25:42 +0900

    test

--- deploy/start.sh
+++ deploy/start.sh
@@ -7,17 +7,38 @@ echo "> build 파일 복사" >> /home/ubuntu/deploy.log
 DEPLOY_PATH=/home/ubuntu/
 cp $BUILD_JAR $DEPLOY_PATH
 
-echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/deploy.log
-CURRENT_PID=$(pgrep -f $JAR_NAME)
+#echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/deploy.log
+#CURRENT_PID=$(pgrep -f $JAR_NAME)
 
-if [ -z $CURRENT_PID ]
-then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/deploy.log
-else
-  echo "> kill -15 $CURRENT_PID"
-  kill -15 $CURRENT_PID
+#if [ -z $CURRENT_PID ]
+#then
+#  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/deploy.log
+#else
+#  echo "> kill -9 $CURRENT_PID"
+#  kill -9 $CURRENT_PID
+#  sleep 5
+#fi
+COUNT=0
+echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/deploy.log
+while :
+do
+  CURRENT_PID=$(pgrep -f $JAR_NAME)
+  if [ -z $CURRENT_PID ]
+  then
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/deploy.log
+    break
+  fi
+
+  if [ $COUNT -eq 0 ]
+  then
+    COUNT=10
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    echo "> 현재 구동중인 애플리케이션을 종료합니다." >> /home/ubuntu/deploy.log
+  fi
   sleep 5
-fi
+
+done
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/deploy.log
