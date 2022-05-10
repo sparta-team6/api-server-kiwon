7b11fe99eaa566749978343e8f026adbb00bb5b2
kiwon kwon <funnykyeon@naver.com>
2022-05-10 15:17:36 +0900

    카카오 인가 처리

--- src/main/java/com/example/githubactiontest/security/WebSecurityConfig.java
+++ src/main/java/com/example/githubactiontest/security/WebSecurityConfig.java
@@ -72,15 +72,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     // CORS 정책 필터
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("http://3.34.133.92:8080/");
-        configuration.addAllowedOrigin("http://localhost:3000/");
-        configuration.addAllowedOrigin("http://localhost:8080/");
-        configuration.addAllowedMethod("");
-        configuration.addAllowedHeader("");
-        configuration.addExposedHeader("Authorization");
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.addExposedHeader("Authorization");
+        source.registerCorsConfiguration("/**", config);
         return source;
     }
 }
